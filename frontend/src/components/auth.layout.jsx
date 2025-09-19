debc7f8a615183c842a4bdf8ddf6c4e36af73609
duttaanirban <duttaanirbanad@gmail.com>
2025-09-19 21:05:12 +0530

    built right component

--- frontend/src/components/auth.layout.jsx
+++ frontend/src/components/auth.layout.jsx
@@ -1,4 +1,5 @@
-import card1 from '../assets/images/card1.webp'
+import card1 from '../assets/images/card1.png'
+import { LuTrendingUpDown } from 'react-icons/lu'
 
 const AuthLayout = ({ children }) => {
   return (
@@ -32,10 +33,16 @@ export default AuthLayout;
 
 const StatsInfoCard = ({ icon, label, value, color }) => {
   return (
-    <div>
+    <div className='flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10'>
       <div
-      className='w-12 h-12 flex items-center'
-      ></div>
+      className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}
+      >
+        {icon}
+      </div>
+      <div className='mt-2'>
+        <h6 className='text-sm text-gray-500 mb-1'>{label}</h6>
+        <span className='text-[20px]'>${value}</span>
+      </div>
     </div>
   )
 }
