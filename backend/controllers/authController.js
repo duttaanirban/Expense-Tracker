e84d3ab2a9e230f6ece26198c3e283e2eb82b73c
duttaanirban <duttaanirbanad@gmail.com>
2025-09-22 21:35:06 +0530

    backend routes created

---
+++ backend/controllers/authController.js
@@ -0,0 +1,12 @@
+
+exports.registerUser = (req, res) => {
+  res.send('Register endpoint');
+};
+
+exports.loginUser = (req, res) => {
+  res.send('Login endpoint');
+};
+
+exports.getUserInfo = (req, res) => {
+  res.send('User info endpoint');
+};
