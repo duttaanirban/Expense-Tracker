5503fb7325c6a974563b4178610ab792c0ebbdaf
duttaanirban <duttaanirbanad@gmail.com>
2025-09-22 21:21:46 +0530

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(cors(
 ));
 
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
