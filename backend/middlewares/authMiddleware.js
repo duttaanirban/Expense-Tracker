6f9dca0ae80bc45a18201df6290edfabe86f9fb6
duttaanirban <duttaanirbanad@gmail.com>
2025-09-24 23:42:15 +0530

    check added

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,6 +7,9 @@ exports.protect = async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        return res.status(401).json({ message: 'Not authorized, no token' });
+      }
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
       next();
