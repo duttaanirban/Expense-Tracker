082a9b2e4daa345b267292b8b66a01718edd0142
duttaanirban <duttaanirbanad@gmail.com>
2025-09-26 20:18:44 +0530

    upload image endpoint created

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,6 +7,7 @@ const {
     loginUser,
     getUserInfo,
     } = require('../controllers/authController');
+const upload = require('../middlewares/uploadMiddleware');
 
 const router = express.Router();
 
@@ -14,4 +15,12 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/getUser', protect, getUserInfo);
 
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({message: "no file uploaded"});
+    }
+    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+    res.status(200).json({imageUrl});
+})
 module.exports = router;
