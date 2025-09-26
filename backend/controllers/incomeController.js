111e22e2d8cd11d412d1a0e96a40d666c53a9f05
duttaanirban <duttaanirbanad@gmail.com>
2025-09-26 23:28:49 +0530

    income schema created

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+const Income = require('../models/Income');
+
+
+//addIncome
+exports.addIncome = async (req, res) => {
+
+};
+
+exports.getAllIncomes = async (req, res) => {
+
+};
+
+exports.deleteIncome = async (req, res) => {
+
+};
+
+exports.downloadIncomeExcel = async (req, res) => {
+
+};
---
+++ backend/models/Income.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const incomeSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    icon: { 
+        type: String,
+    },
+    source: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Income', incomeSchema);
