92d9d35cd29af43e9b9f7c3db53dbb28b29816c3
duttaanirban <duttaanirbanad@gmail.com>
2025-09-29 18:33:46 +0530

    custom hook created

---
+++ frontend/src/hooks/useUserAuth.jsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from "react";
+import { UserContext } from "../context/userContext";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../utils/axios";
+import { API_PATHS } from "../utils/apiPaths";
+
+export const useUserAuth = () => {
+    const {user, updateUser, clearUser} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) return; // User is already set in context
+
+        let isMounted = true; // To prevent state updates if the component is unmounted
+
+        const fetchUserInfo = async() => {
+            try {
+                const response = await axiosInstance.get(API_PATHS.AUTH.GET_USER_INFO);
+
+                if (isMounted && response.data) {
+                    updateUser(response.data);
+                }
+            } catch (error) {
+                console.error("Error fetching user info:", error);
+                if (isMounted) {
+                    clearUser();
+                    navigate("/login");
+                }
+            }
+        }
+
+        fetchUserInfo();
+        
+        return () => {
+            isMounted = false; // Cleanup flag on unmount
+        };
+    }, [user, updateUser, clearUser, navigate]);
+};
